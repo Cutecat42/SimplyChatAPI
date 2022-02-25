7b38458f392a29b9d200a7c97268acaf9d6ea56a
unknown <musical_kitty@hotmail.com>
2022-02-25 13:57:43 -0700

    Set up basic Firebase endpoint

--- src/endpoints/endpoints.js
+++ src/endpoints/endpoints.js
@@ -5,6 +5,13 @@
 const express = require("express");
 const { getPublicMessage, getProtectedMessage, getUser } = require("./endpointFuncs");
 const { checkJwt } = require("../authz/check-jwt");
+const serviceAccount = require('../firebase/firebase-key');
+const firebaseAdmin = require('firebase-admin');
+
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(serviceAccount),
+    databaseURL: `https://${serviceAccount.project_id}.firebaseio.com`
+});
 
 /**
  * Router Definition
@@ -31,6 +38,21 @@ endpointsRouter.get("/protected-message", checkJwt, (req, res) => {
 endpointsRouter.get("/user", checkJwt, (req, res) => {
     const message = getUser(req.headers.authorization);
     res.status(200).send(message);
+});
+
+endpointsRouter.get('/firebase', checkJwt, async (req, res) => {
+    const { sub: uid } = req.user;
+
+    try {
+        const firebaseToken = await firebaseAdmin.auth().createCustomToken(uid);
+        res.json({ firebaseToken });
+        console.log(firebaseToken);
+    } catch (err) {
+        res.status(500).send({
+            message: 'Something went wrong acquiring a Firebase token.',
+            error: err
+        });
+    }
 });
 
 module.exports = {
